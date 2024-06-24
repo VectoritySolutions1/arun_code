a0262fc83c778ddf4fe64a4a87ab3cf8b56efe29
VectoritySolutions1 <123560301+VectoritySolutions1@users.noreply.github.com>
2024-06-24 18:25:25 +0530

    Update sql_scripth.py

--- sql_scripth.py
+++ sql_scripth.py
@@ -1,32 +1,32 @@
 def identify_sql_type(sql_raw):
     sql_type = None
 
-    # Check for INSERT /*+ APPEND */ pattern
-    if re.search(r'\bINSERT\s*/\*\+\s*APPEND\s*\*/\b', sql_raw, re.IGNORECASE):
-        sql_type = 'INSERT /*+ APPEND */'
+    # Check for INSERT pattern
+    if re.search(r'^\s*INSERT\b', sql_raw, re.IGNORECASE | re.MULTILINE):
+        sql_type = 'INSERT'
     
     # Check for SELECT pattern
-    elif re.search(r'\bSELECT\b', sql_raw, re.IGNORECASE):
+    elif re.search(r'^\s*SELECT\b', sql_raw, re.IGNORECASE | re.MULTILINE):
         sql_type = 'SELECT'
     
     # Check for UPDATE pattern
-    elif re.search(r'\bUPDATE\b', sql_raw, re.IGNORECASE):
+    elif re.search(r'^\s*UPDATE\b', sql_raw, re.IGNORECASE | re.MULTILINE):
         sql_type = 'UPDATE'
     
     # Check for DELETE pattern
-    elif re.search(r'\bDELETE\b', sql_raw, re.IGNORECASE):
+    elif re.search(r'^\s*DELETE\b', sql_raw, re.IGNORECASE | re.MULTILINE):
         sql_type = 'DELETE'
     
     # Check for TRUNCATE pattern
-    elif re.search(r'\bTRUNCATE\b', sql_raw, re.IGNORECASE):
+    elif re.search(r'^\s*TRUNCATE\b', sql_raw, re.IGNORECASE | re.MULTILINE):
         sql_type = 'TRUNCATE'
     
     # Check for MERGE pattern
-    elif re.search(r'\bMERGE\b', sql_raw, re.IGNORECASE):
+    elif re.search(r'^\s*MERGE\b', sql_raw, re.IGNORECASE | re.MULTILINE):
         sql_type = 'MERGE'
     
     # Check for CREATE TABLE pattern
-    elif re.search(r'\bCREATE\s+TABLE\b', sql_raw, re.IGNORECASE):
+    elif re.search(r'^\s*CREATE\s+TABLE\b', sql_raw, re.IGNORECASE | re.MULTILINE):
         sql_type = 'CREATE TABLE'
     
     # Add more patterns as needed for other SQL operations
